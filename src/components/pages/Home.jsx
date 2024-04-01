0c031e32802948cd5f0d98f1bf4db9e3e950443e
Winder Sleam Segura Melchor <windersleamseguramelchor@MacBook-Pro-de-Winder.local>
2024-04-01 16:08:55 -0600

    Home page layout desing

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,11 +1,31 @@
-
-
 export const Home = () => {
   return (
     <>
-        <nav>navigation</nav>
-        <main>main</main>
-        <footer>footer</footer>
+      <nav>
+        <div className="bg-lime-800 text-white h-12 content-center rounded-md">
+          <ul className="flex flex-row ml-8">
+            <li className="basis-1/12 hover:cursor-pointer hover:text-gray-400">
+              Home
+            </li>
+            <li className="basis-1/12 hover:cursor-pointer hover:text-gray-400">
+              Contact
+            </li>
+            <li className="basis-1/12 hover:cursor-pointer hover:text-gray-400">
+              About
+            </li>
+          </ul>
+        </div>
+      </nav>
+      <main className="bg-slate-200 rounded-md p-3">
+        <div className="h-screen">
+          <p>main content</p>
+        </div>
+      </main>
+      <footer className="bg-lime-800 text-whiterounded-md h-16 text-white">
+        <div className="fex flex-row text-center pt-4">
+            <p className="text-xs">©{new Date().getFullYear()} All Rights Reserved.</p>
+        </div>
+      </footer>
     </>
-  )
-}
+  );
+};
